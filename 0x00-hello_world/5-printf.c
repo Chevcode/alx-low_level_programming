6b28ddcc56547421fc0c843cdbace895df14cbda
chevcode <chevcode@gmail.com>
2023-06-15 19:09:13 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entery
- * Return: Always 0
+ * main - program that prints using printf
+ * Return: 0
  */
 int main(void)
-
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
